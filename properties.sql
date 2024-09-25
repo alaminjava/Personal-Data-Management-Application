825acafca207c2df65bcc4260fa0863b1868ee62
Md Alamin Hossain <58477678+alaminjava@users.noreply.github.com>
2024-09-25 12:42:28 -0700

    Create properties.sql

---
+++ properties.sql
@@ -0,0 +1,9 @@
+spring.datasource.url=jdbc:mysql://localhost:3306/personal_db?useSSL=false
+spring.datasource.username=root
+spring.datasource.password=root_password
+spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
+spring.mvc.view.prefix=/WEB-INF/views/
+spring.mvc.view.suffix=.html
